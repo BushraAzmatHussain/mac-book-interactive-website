9348abc100081c895a45c9f4ae309e25f9300fd5
BushraAzmatHussain <bushrahussain407@gmail.com>
2021-08-21 18:39:05 +0600

    final push

--- js/app.js
+++ js/app.js
@@ -1,35 +1,101 @@
-document.getElementById('sixteenGBMemory').addEventListener('click',function(){
+function memory(memorySixteen){
     const memoryCost = document.getElementById('extraMemoryPrice');
-    const memoryCostInnertext = memoryCost.innerText;
-    memoryCost.innerText = parseInt(180);
+    if(memorySixteen == true){
+        memoryCost.innerText = parseFloat(180);
+    }
+    else if(memorySixteen == false){
+        memoryCost.innerText = parseFloat(0);
+    }
+
+}
+
+
+
+function storage(SSD){
+    const storageCost = document.getElementById('extraStorageCost');
+    if(SSD == true){
+        storageCost.innerText = parseFloat(180);
+    }
+    else if(SSD == false){
+        storageCost.innerText = parseFloat(100);
+    }
+    else{
+        storageCost.innerText = parseFloat(0);
+    }
+}
+
+function delivery(deliveryCharge){
+    const deliveryCost = document.getElementById('delivery-cost');
+    if(deliveryCharge == true){
+        deliveryCost.innerText = parseFloat(20);
+    }
+    else if(deliveryCharge == false){
+        deliveryCost.innerText = parseFloat(0);
+    }
+}
+
+
+function total(){
+    const bestPrice = document.getElementById('best-price').innerText;
+    const memory = document.getElementById('extraMemoryPrice').innerText;
+    const storage = document.getElementById('extraStorageCost').innerText;
+    const delivery = document.getElementById('delivery-cost').innerText;
+     
+    const totalPrice = parseFloat(bestPrice) + parseFloat(memory) + parseFloat(storage) + parseFloat(delivery);
+
+    document.getElementById('total-price').innerText = totalPrice;
+    document.getElementById('final-price').innerText = totalPrice ;
+
+
+}
+
+
+  //update total
+
+  document.getElementById('apply').addEventListener("click", function () {
+      const promoCodeField = document.getElementById('input');
+      const finalPrice = document.getElementById('final-price');
+      const totalCost = document.getElementById('total-price').innerText;
+      const totalPrice = parseInt(totalCost);
+      if(promoCodeField.value == 'stevekaku'){
+          finalPrice.innerText = (totalPrice - (totalPrice*0.2));
+        }
+        
+        else{
+            alert('Please enter valid promo code to get 20% discount ');
+            finalPrice.innerText = totalPrice;
+        }
+        promoCodeField.value ='';
+    });
+
+document.getElementById('sixteenGBMemory').addEventListener('click',function(){
+   memory(true);
+   total();
 });
 document.getElementById('eightGBMemory').addEventListener('click',function(){
-    const memoryCost = document.getElementById('extraMemoryPrice');
-    const memoryCostInnertext = memoryCost.innerText;
-    memoryCost.innerText = parseInt(0);
+
+    memory(false);
+    total();
 });
 document.getElementById('twoFiftySix').addEventListener('click',function(){
-    const storageCost = document.getElementById('extraStorageCost');
-    const storageCostInnertext = storageCost.innerText;
-    storageCost.innerText = parseInt(0);
+    storage();
+    total();
 });
 document.getElementById('fiveTwelve').addEventListener('click',function(){
-    const storageCost = document.getElementById('extraStorageCost');
-    const storageCostInnertext = storageCost.innerText;
-    storageCost.innerText = parseInt(100);
+    storage(false);
+    total();
 });
 document.getElementById('oneTeraBite').addEventListener('click',function(){
-    const storageCost = document.getElementById('extraStorageCost');
-    const storageCostInnertext = storageCost.innerText;
-    storageCost.innerText = parseInt(180);
+    storage(true);
+    total();
 });
 document.getElementById('delivery-zero').addEventListener('click',function(){
-    const deliveryCost = document.getElementById('delivery-cost');
-    const deliveryCostInnertext = deliveryCost.innerText;
-    deliveryCost.innerText = parseInt(0);
+    delivery(false);
+    total();
+
 });
 document.getElementById('delivery-twenty').addEventListener('click',function(){
-    const deliveryCost = document.getElementById('delivery-cost');
-    const deliveryCostInnertext = deliveryCost.innerText;
-    deliveryCost.innerText = parseInt(20);
+    delivery(true);
+    total();
 });
+
